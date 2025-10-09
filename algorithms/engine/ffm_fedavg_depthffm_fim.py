8f333a047d309e9d57b9b7802c9d4e3a6d8ad512
Liam Lian <liamyaolian@gmail.com>
2025-10-09 10:08:12 -0400

    remove unused

--- algorithms/engine/ffm_fedavg_depthffm_fim.py
+++ algorithms/engine/ffm_fedavg_depthffm_fim.py
@@ -34,39 +34,39 @@ def test_collate_fn(examples):
         labels = torch.tensor([example["fine_label"] for example in examples])
     return {"pixel_values": pixel_values, "labels": labels}
 
-@dataclass
-class DataCollatorForMultipleChoice:
-    """
-    Data collator that will dynamically pad the inputs for multiple choice received.
-    """
+# @dataclass
+# class DataCollatorForMultipleChoice:
+#     """
+#     Data collator that will dynamically pad the inputs for multiple choice received.
+#     """
 
-    tokenizer: PreTrainedTokenizerBase
-    padding: Union[bool, str, PaddingStrategy] = True
-    max_length: Optional[int] = None
-    pad_to_multiple_of: Optional[int] = None
+#     tokenizer: PreTrainedTokenizerBase
+#     padding: Union[bool, str, PaddingStrategy] = True
+#     max_length: Optional[int] = None
+#     pad_to_multiple_of: Optional[int] = None
 
-    def __call__(self, features):
-        label_name = "correct_answer_num"
-        labels = [torch.tensor(int(feature[label_name])-1) for feature in features]
-        batch_size = len(features)
-        num_choices = len(features[0]["input_ids"])
+#     def __call__(self, features):
+#         label_name = "correct_answer_num"
+#         labels = [torch.tensor(int(feature[label_name])-1) for feature in features]
+#         batch_size = len(features)
+#         num_choices = len(features[0]["input_ids"])
 
-        flattened_features = [
-            [{k: v[i] for k, v in feature.items() if k in ['input_ids', 'token_type_ids', 'attention_mask']} for i in range(num_choices)] for feature in features
-        ]
-        flattened_features = sum(flattened_features, [])
+#         flattened_features = [
+#             [{k: v[i] for k, v in feature.items() if k in ['input_ids', 'token_type_ids', 'attention_mask']} for i in range(num_choices)] for feature in features
+#         ]
+#         flattened_features = sum(flattened_features, [])
 
-        batch = self.tokenizer.pad(
-            flattened_features,
-            padding=self.padding,
-            max_length=self.max_length,
-            pad_to_multiple_of=self.pad_to_multiple_of,
-            return_tensors="pt",
-        )
+#         batch = self.tokenizer.pad(
+#             flattened_features,
+#             padding=self.padding,
+#             max_length=self.max_length,
+#             pad_to_multiple_of=self.pad_to_multiple_of,
+#             return_tensors="pt",
+#         )
 
-        batch = {k: v.view(batch_size, num_choices, -1) for k, v in batch.items()}
-        batch["labels"] = torch.tensor(labels, dtype=torch.int64)
-        return batch
+#         batch = {k: v.view(batch_size, num_choices, -1) for k, v in batch.items()}
+#         batch["labels"] = torch.tensor(labels, dtype=torch.int64)
+#         return batch
 
 def ffm_fedavg_depthffm_fim(args):
     ################################### hyperparameter setup ########################################
--- test.py
+++ test.py
@@ -9,9 +9,9 @@ import numpy as np
 
 from accelerate import Accelerator
 from accelerate import DistributedDataParallelKwargs
-from transformers.tokenization_utils_base import PreTrainedTokenizerBase, PaddingStrategy
-from typing import Optional, Union
-from dataclasses import dataclass
+# from transformers.tokenization_utils_base import PreTrainedTokenizerBase, PaddingStrategy
+# from typing import Optional, Union
+# from dataclasses import dataclass
 
 def collate_fn(examples):
     pixel_values = torch.stack([example["pixel_values"] for example in examples])
@@ -21,39 +21,39 @@ def collate_fn(examples):
         labels = torch.tensor([example["fine_label"] for example in examples])
     return {"pixel_values": pixel_values, "labels": labels}
 
-@dataclass
-class DataCollatorForMultipleChoice:
-    """
-    Data collator that will dynamically pad the inputs for multiple choice received.
-    """
+# @dataclass
+# class DataCollatorForMultipleChoice:
+#     """
+#     Data collator that will dynamically pad the inputs for multiple choice received.
+#     """
 
-    tokenizer: PreTrainedTokenizerBase
-    padding: Union[bool, str, PaddingStrategy] = True
-    max_length: Optional[int] = None
-    pad_to_multiple_of: Optional[int] = None
+#     tokenizer: PreTrainedTokenizerBase
+#     padding: Union[bool, str, PaddingStrategy] = True
+#     max_length: Optional[int] = None
+#     pad_to_multiple_of: Optional[int] = None
 
-    def __call__(self, features):
-        label_name = "correct_answer_num"
-        labels = [torch.tensor(int(feature[label_name])-1) for feature in features]
-        batch_size = len(features)
-        num_choices = len(features[0]["input_ids"])
+#     def __call__(self, features):
+#         label_name = "correct_answer_num"
+#         labels = [torch.tensor(int(feature[label_name])-1) for feature in features]
+#         batch_size = len(features)
+#         num_choices = len(features[0]["input_ids"])
 
-        flattened_features = [
-            [{k: v[i] for k, v in feature.items() if k in ['input_ids', 'token_type_ids', 'attention_mask']} for i in range(num_choices)] for feature in features
-        ]
-        flattened_features = sum(flattened_features, [])
+#         flattened_features = [
+#             [{k: v[i] for k, v in feature.items() if k in ['input_ids', 'token_type_ids', 'attention_mask']} for i in range(num_choices)] for feature in features
+#         ]
+#         flattened_features = sum(flattened_features, [])
 
-        batch = self.tokenizer.pad(
-            flattened_features,
-            padding=self.padding,
-            max_length=self.max_length,
-            pad_to_multiple_of=self.pad_to_multiple_of,
-            return_tensors="pt",
-        )
+#         batch = self.tokenizer.pad(
+#             flattened_features,
+#             padding=self.padding,
+#             max_length=self.max_length,
+#             pad_to_multiple_of=self.pad_to_multiple_of,
+#             return_tensors="pt",
+#         )
 
-        batch = {k: v.view(batch_size, num_choices, -1) for k, v in batch.items()}
-        batch["labels"] = torch.tensor(labels, dtype=torch.int64)
-        return batch
+#         batch = {k: v.view(batch_size, num_choices, -1) for k, v in batch.items()}
+#         batch["labels"] = torch.tensor(labels, dtype=torch.int64)
+#         return batch
 
 def test_vit(model, dataset, args, t):
     # Eval for each round
