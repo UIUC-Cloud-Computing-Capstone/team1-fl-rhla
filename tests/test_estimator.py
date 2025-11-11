ea184c72a167c7026b2319070ea4a1826193eb6c
Liam Lian <liamyaolian@gmail.com>
2025-11-10 20:43:06 -0500

    adjust summary depth

--- tests/test_estimator.py
+++ tests/test_estimator.py
@@ -96,7 +96,7 @@ class TestRankEstimator(unittest.TestCase):
             modules_to_save=["classifier"],
         )
         model = get_peft_model(model, config)
-        summary(model, input_size=(1, 3, 224, 224), col_names=["input_size", "output_size", "num_params", "trainable"])
+        summary(model, input_size=(1, 3, 224, 224), col_names=["input_size", "output_size", "num_params", "trainable"], depth=11)
 
         
 
