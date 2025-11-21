8e22fbda6fef60c818943803468501767d7519fa
youye <19175790+youyexie@users.noreply.github.com>
2025-11-21 09:48:11 -0800

    yamls

---
+++ run-cifar100-LEGEND.sh
@@ -0,0 +1,7 @@
+##############
+## CIFAR100 ##
+##############
+
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/cifar100_vit_lora/LEGEND/LEGEND-iid.yaml'
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/cifar100_vit_lora/LEGEND/LEGEND-noniid-10.yaml' 
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/cifar100_vit_lora/LEGEND/LEGEND-noniid-20.yaml'
