9241cf0e3a466930cf9d41624d94e495c18002ec
youye <19175790+youyexie@users.noreply.github.com>
2025-12-03 00:15:15 -0800

    ledgar non-iid yaml

---
+++ run-ledgar-non-iid-20.sh
@@ -0,0 +1,30 @@
+##############
+## ledgar   ##
+##############
+
+# exclusive
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/ledgar_bert_lora/Exclusive/Exclusive-rank24-noniid-pat_20_dir.yaml'
+
+# fedhello
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/ledgar_bert_lora/FedHello/FedHello_noniid-pat_20_dir-noprior-s50-e50.yaml'
+
+#ours
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/ledgar_bert_lora/Ours/alternating-training-warm20-double-rank-int-no-rank-vary-noniid-20.yaml'
+
+#straggler
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/ledgar_bert_lora/Straggler/straggler-rank24-noniid-pat_20_dir.yaml'
+
+#fedit
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/ledgar_bert_lora/FedIT/FedIT-noniid-pat_20_dir.yaml'
+
+#ffa-lora
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/ledgar_bert_lora/FFA-LoRA/FFA-LORA-image-b-only-noniid-20.yaml'
+
+#LEGEND
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/ledgar_bert_lora/LEGEND/LEGEND-noniid-20.yaml'
+
+#lokr
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/ledgar_bert_lora/LOKR/LOKR-noniid-20.yaml'
+
+
+
