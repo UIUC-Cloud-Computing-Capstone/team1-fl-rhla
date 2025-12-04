eedffe78d3d6b5dff1b682a05faad8dc6c9a226a
youye <19175790+youyexie@users.noreply.github.com>
2025-12-03 23:53:11 -0800

    varying alpha

---
+++ run-cifar100-non-iid-10-ablation-alpha.sh
@@ -0,0 +1,21 @@
+##############
+## CIFAR100 ##
+##############
+
+# 0.0
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/ablation/diff-alpha-cifar100-non-iid-10/alternating-training-warm20-double-rank-int-no-rank-vary-rank24-0dot0.yaml'
+
+# 0.2
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/ablation/diff-alpha-cifar100-non-iid-10/alternating-training-warm20-double-rank-int-no-rank-vary-rank24-0dot2.yaml'
+
+# 0.4
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/ablation/diff-alpha-cifar100-non-iid-10/alternating-training-warm20-double-rank-int-no-rank-vary-rank24-0dot4.yaml'
+
+# 0.6
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/ablation/diff-alpha-cifar100-non-iid-10/alternating-training-warm20-double-rank-int-no-rank-vary-rank24-0dot6.yaml'
+
+# 0.8
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/ablation/diff-alpha-cifar100-non-iid-10/alternating-training-warm20-double-rank-int-no-rank-vary-rank24-0dot8.yaml'
+
+# 1.0
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/ablation/diff-alpha-cifar100-non-iid-10/alternating-training-warm20-double-rank-int-no-rank-vary-rank24-1dot0.yaml'
---
+++ run-cifar100-non-iid-20-ablation.sh
@@ -0,0 +1,24 @@
+##############
+## CIFAR100 ##
+##############
+
+# rank16
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/ablation/noniid-20-cifar/alternating-training-warm20-double-rank-int-no-rank-vary-rank16.yaml'
+
+# rank24
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/ablation/noniid-20-cifar/alternating-training-warm20-double-rank-int-no-rank-vary-rank24.yaml'
+
+# nosvd
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/ablation/noniid-20-cifar/alternating-training-warm20-double-rank-int-no-rank-vary-rank24-nosvd.yaml'
+
+# no alternating
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/ablation/noniid-20-cifar/alternating-training-warm20-double-rank-int-no-rank-vary-rank24-noalternating.yaml'
+
+# no rank selection
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/ablation/noniid-20-cifar/alternating-training-warm20-double-rank-int-no-rank-vary-rank24-norankselection.yaml'
+
+# rank8
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/ablation/noniid-20-cifar/alternating-training-warm20-double-rank-int-no-rank-vary-rank8.yaml'
+
+# rank32
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/ablation/noniid-20-cifar/alternating-training-warm20-double-rank-int-no-rank-vary-rank32.yaml'
