f13fcad20f058feb8af6f20d85e9567f0baca313
Liam Lian <liamyaolian@gmail.com>
2025-12-13 23:06:14 -0500

    update test_get_rank_for_all_client_groups_ours

--- tests/test_estimator.py
+++ tests/test_estimator.py
@@ -91,10 +91,10 @@ class TestRankEstimator(unittest.TestCase):
         # resource heterogeneity
         # all clients belong to 3 heterogeneous groups. Each group has different resource limitation.
         args.gpu_memory_size_for_each_group_in_GB = [2, 4, 8]
-        args.avg_upload_network_speed_for_each_group_in_Mbps = [1.5, 2, 2.5]
+        args.avg_upload_network_speed_for_each_group_in_Mbps = [1, 1.5, 2]
         args.avg_download_network_speed_for_each_group_in_Mbps = [10, 50, 50]
-        args.desired_uploading_time_for_each_group_in_seconds = [60, 60, 60]
-        args.desired_downloading_time_for_each_group_in_seconds = [60, 60, 60]
+        args.desired_uploading_time_for_each_group_in_seconds = [15, 15, 15]
+        args.desired_downloading_time_for_each_group_in_seconds = [30, 30, 30]
         args.heterogeneous_group = [1/3, 1/3, 1/3] # 1/3 of clients belong to each group.
 
         # model
